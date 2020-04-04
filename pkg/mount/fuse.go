40df430dac74bf74df816bd4659358702ef09ca7
JT Olio <hello@jtolio.com>
2020-04-04 16:56:15 -0600

    import hanwen/go-fuse/v2

--- pkg/mount/fuse.go
+++ pkg/mount/fuse.go
@@ -2,11 +2,31 @@ package mount
 
 import (
 	"context"
-	"fmt"
+	"syscall"
+
+	"github.com/hanwen/go-fuse/v2/fs"
+	"github.com/hanwen/go-fuse/v2/fuse"
 
 	"github.com/jtolds/jam/pkg/session"
 )
 
+type fuseNode struct {
+	fs.Inode
+}
+
+var _ fs.InodeEmbedder = (*fuseNode)(nil)
+var _ fs.NodeLookuper = (*fuseNode)(nil)
+
+func (n *fuseNode) Lookup(ctx context.Context, name string, out *fuse.EntryOut) (
+	*fs.Inode, syscall.Errno) {
+	return n.NewInode(ctx, &fuseNode{}, fs.StableAttr{Mode: syscall.S_IFDIR}), 0
+}
+
 func Mount(ctx context.Context, snap session.Snapshot, target string) error {
-	return fmt.Errorf("TODO: unimplemented")
+	server, err := fs.Mount(target, &fuseNode{}, nil)
+	if err != nil {
+		return err
+	}
+	server.Serve()
+	return nil
 }
